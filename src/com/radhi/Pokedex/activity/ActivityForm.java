11cab29f51c460a6665c88167ad0d67bc19043d3
Acrophobic <m.radhi.f@gmail.com>
2014-01-05 13:56:01 +0700

    Minor change

--- src/com/radhi/Pokedex/activity/ActivityForm.java
+++ src/com/radhi/Pokedex/activity/ActivityForm.java
@@ -1,6 +1,5 @@
 package com.radhi.Pokedex.activity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,10 +8,12 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import com.radhi.Pokedex.R;
 import com.radhi.Pokedex.fragment.PokemonAppearance;
+import com.radhi.Pokedex.fragment.PokemonMove;
 import com.radhi.Pokedex.fragment.PokemonStat;
 import com.radhi.Pokedex.object.Pokemon;
 import com.radhi.Pokedex.other.PagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -33,36 +34,36 @@ public class ActivityForm extends FragmentActivity {
         mPage.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,ID,Form_ID);
     }
 
-    private class makePage extends AsyncTask<String, Void, List<Fragment>> {
-        ProgressDialog ringProgressDialog;
+    private class makePage extends AsyncTask<String, Void, Void> {
+        List<Fragment> fragmentList;
+        List<String> titleList;
 
         @Override
-        protected void onPreExecute() {
-            ringProgressDialog = ProgressDialog.show(ActivityForm.this, "Please wait...", "Loading data...");
-        }
-
-        @Override
-        protected List<Fragment> doInBackground(String... ID) {
+        protected Void doInBackground(String... ID) {
             Pokemon pokemon = new Pokemon(getBaseContext(),ID[0]);
             Bundle args = new Bundle();
             args.putParcelable(ActivityMain.POKEMON_DATA,pokemon);
             args.putString(ActivityMain.POKEMON_FORM_ID,ID[1]);
 
-            List<Fragment> fragmentList = new Vector<Fragment>();
+            fragmentList = new Vector<Fragment>();
             fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonAppearance.class.getName(), args));
             fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonStat.class.getName(), args));
+            fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonMove.class.getName(), args));
 
-            return fragmentList;
+            titleList = new ArrayList<String>();
+            titleList.add("APPEARANCE");
+            titleList.add("STATS");
+            titleList.add("MOVE");
+
+            return null;
         }
 
         @Override
-        protected void onPostExecute(List<Fragment> result) {
-            ringProgressDialog.dismiss();
-            PagerAdapter mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), result);
+        protected void onPostExecute(Void result) {
+            PagerAdapter mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), fragmentList, titleList);
             ViewPager mPager = (ViewPager) findViewById(R.id.pager);
             mPager.setAdapter(mPagerAdapter);
             mPager.setOffscreenPageLimit(2);
-            getWindow().setBackgroundDrawable(null);
         }
     }
 }
