d3a104d303a97e02e75d9447602c75affe3e070a
Acrophobic <m.radhi.f@gmail.com>
2014-02-13 16:37:09 +0700

    Fragment PokemonName.

--- src/com/radhi/Pokedex/fragment/PokemonName.java
+++ src/com/radhi/Pokedex/fragment/PokemonName.java
@@ -1,193 +1,300 @@
 package com.radhi.Pokedex.fragment;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
+import android.support.v4.app.Fragment;
+import android.util.SparseArray;
+import android.view.*;
 import android.widget.*;
 import com.radhi.Pokedex.R;
 import com.radhi.Pokedex.adapter.ListNameAdapter;
 import com.radhi.Pokedex.other.Database;
+import com.radhi.Pokedex.other.Other.pokemonInterface;
 
 public class PokemonName extends Fragment {
     private Activity activity;
     private Database DB;
-    private OnPokemonSelectedListener pokemonSelectedListener;
-    private EditText edPokemon;
-    private ListView listPokemon;
-    private LinearLayout boxSearch;
-    private Boolean searching = false;
 
-    private String Name = "%",
-            Generation = "0",
-            Color = "0",
-            Type1 = "0",
-            Type2 = "0";
-    private Boolean isBaby = false,
-            hasGenderDiff = false;
+    private GridView gridPokemon;
+    private TextView txtShading;
+    private ScrollView scrollFilter;
+
+    private TextView txtFilterGeneration;
+    private SparseArray<CheckBox> listChkGeneration;
+    private boolean genVisible = false;
+
+    private TextView txtFilterType;
+    private SparseArray<CheckBox> listChkType;
+    private boolean typeVisible = false;
+
+    private TextView txtFilterColor;
+    private SparseArray<CheckBox> listChkColor;
+    private boolean colorVisible = false;
+
+    private CheckBox chkBaby, chkGenderDiff;
+    private TextView txtClearFilter;
+
+    private String Name = "", Generation = "", Color = "", Type = "";
+    private Boolean isBaby = false, hasGenderDiff = false;
+
+    private pokemonInterface pokemonInterface;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_pokemon_name, container, false);
         DB = new Database(activity);
+        View view = inflater.inflate(R.layout.pokemon_name, container, false);
+        setHasOptionsMenu(true);
+        setComponentName(view);
+        makeList();
 
-        edPokemon = (EditText) view.findViewById(R.id.txtPokemonName);
-        listPokemon = (ListView) view.findViewById(R.id.listPokemon);
-        boxSearch = (LinearLayout) view.findViewById(R.id.boxSearchPokemon);
+        setCLickListenerOnFilter();
 
-        final TextView txtClear = (TextView) view.findViewById(R.id.txtClear);
-        final Spinner spinGeneration = (Spinner) view.findViewById(R.id.spinGeneration);
-        final Spinner spinColor = (Spinner) view.findViewById(R.id.spinColor);
-        final Spinner spinType1 = (Spinner) view.findViewById(R.id.spinType1);
-        final Spinner spinType2 = (Spinner) view.findViewById(R.id.spinType2);
-        final CheckBox checkIsBaby = (CheckBox) view.findViewById(R.id.checkIsBaby);
-        final CheckBox checkHasGenderDiff = (CheckBox) view.findViewById(R.id.checkHasGenderDiff);
-        final TextView txtTapClear = (TextView) view.findViewById(R.id.txtTap);
-
-        updateList();
-
-        listPokemon.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        gridPokemon.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                pokemonSelectedListener.onPokemonSelected(listPokemon.getItemAtPosition(position).toString());
+                String[] row = gridPokemon.getItemAtPosition(position).toString().split(Database.SPLIT);
+                pokemonInterface.pokemonSelected(row[0]);
             }
         });
 
-        edPokemon.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0) txtClear.setVisibility(View.VISIBLE);
-                else txtClear.setVisibility(View.GONE);
-
-                Name = "%" + s.toString() + "%";
-                updateList();
-            }
-        });
-
-        txtClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {edPokemon.setText("");}
-        });
-
-        spinGeneration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Generation = String.valueOf(position);
-                updateList();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-
-        spinColor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Color = String.valueOf(position);
-                updateList();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-
-        spinType1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Type1 = String.valueOf(position);
-                updateList();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-
-        spinType2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Type2 = String.valueOf(position);
-                updateList();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-
-        checkIsBaby.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isBaby = isChecked;
-                updateList();
-            }
-        });
-
-        checkHasGenderDiff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                hasGenderDiff = isChecked;
-                updateList();
-            }
-        });
-
-        txtTapClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                spinGeneration.setSelection(0);
-                spinColor.setSelection(0);
-                spinType1.setSelection(0);
-                spinType2.setSelection(0);
-                checkIsBaby.setChecked(false);
-                checkHasGenderDiff.setChecked(false);
-            }
-        });
+        pokemonInterface = (pokemonInterface) activity;
 
         return view;
-    }
-
-    public interface OnPokemonSelectedListener {
-        public void onPokemonSelected(String rowData);
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         this.activity = getActivity();
-        pokemonSelectedListener = (OnPokemonSelectedListener) activity;
     }
 
-    public void showSearch() {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_pokemon_name, menu);
 
-        if (boxSearch.getVisibility() == View.GONE) {
-            boxSearch.setVisibility(View.VISIBLE);
-            edPokemon.requestFocus();
-            searching = true;
-        }
-        else {
-            imm.hideSoftInputFromWindow(edPokemon.getWindowToken(), 0);
-            boxSearch.setVisibility(View.GONE);
-            searching = false;
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Pokemon's name");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String txt) {
+                Name = txt;
+                setFilter();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_filter:
+                toggleFilterArea();
+                return true;
+            default:
+                return false;
         }
     }
 
-    public boolean isSearching() {return searching;}
-
-    private void updateList() {
+    private void makeList() {
         ListNameAdapter adapter = new ListNameAdapter(activity,
-                DB.getPokemonList(Name, Generation, Color, Type1, Type2, isBaby, hasGenderDiff));
-        listPokemon.setAdapter(adapter);
+                DB.getPokemonList(Name, Generation, Color, Type, isBaby, hasGenderDiff));
+        gridPokemon.setAdapter(adapter);
+    }
+
+    private void setComponentName(View v) {
+        gridPokemon = (GridView) v.findViewById(R.id.listPokemon);
+        txtShading = (TextView) v.findViewById(R.id.shadingView);
+        scrollFilter = (ScrollView) v.findViewById(R.id.scrollFilter);
+
+        txtFilterGeneration = (TextView) v.findViewById(R.id.txtFilterGeneration);
+        listChkGeneration = new SparseArray<CheckBox>();
+        listChkGeneration.put(0, (CheckBox) v.findViewById(R.id.chkGeneration1));
+        listChkGeneration.put(1, (CheckBox) v.findViewById(R.id.chkGeneration2));
+        listChkGeneration.put(2, (CheckBox) v.findViewById(R.id.chkGeneration3));
+        listChkGeneration.put(3, (CheckBox) v.findViewById(R.id.chkGeneration4));
+        listChkGeneration.put(4, (CheckBox) v.findViewById(R.id.chkGeneration5));
+        listChkGeneration.put(5, (CheckBox) v.findViewById(R.id.chkGeneration6));
+
+        txtFilterType = (TextView) v.findViewById(R.id.txtFilterType);
+        listChkType = new SparseArray<CheckBox>();
+        listChkType.put(0, (CheckBox) v.findViewById(R.id.chkTypeNormal));
+        listChkType.put(1, (CheckBox) v.findViewById(R.id.chkTypeFighting));
+        listChkType.put(2, (CheckBox) v.findViewById(R.id.chkTypeFlying));
+        listChkType.put(3, (CheckBox) v.findViewById(R.id.chkTypePoison));
+        listChkType.put(4, (CheckBox) v.findViewById(R.id.chkTypeGround));
+        listChkType.put(5, (CheckBox) v.findViewById(R.id.chkTypeRock));
+        listChkType.put(6, (CheckBox) v.findViewById(R.id.chkTypeBug));
+        listChkType.put(7, (CheckBox) v.findViewById(R.id.chkTypeGhost));
+        listChkType.put(8, (CheckBox) v.findViewById(R.id.chkTypeSteel));
+        listChkType.put(9, (CheckBox) v.findViewById(R.id.chkTypeFire));
+        listChkType.put(10, (CheckBox) v.findViewById(R.id.chkTypeWater));
+        listChkType.put(11, (CheckBox) v.findViewById(R.id.chkTypeGrass));
+        listChkType.put(12, (CheckBox) v.findViewById(R.id.chkTypeElectric));
+        listChkType.put(13, (CheckBox) v.findViewById(R.id.chkTypePsychic));
+        listChkType.put(14, (CheckBox) v.findViewById(R.id.chkTypeIce));
+        listChkType.put(15, (CheckBox) v.findViewById(R.id.chkTypeDragon));
+        listChkType.put(16, (CheckBox) v.findViewById(R.id.chkTypeDark));
+        listChkType.put(17, (CheckBox) v.findViewById(R.id.chkTypeFairy));
+
+        txtFilterColor = (TextView) v.findViewById(R.id.txtFilterColor);
+        listChkColor = new SparseArray<CheckBox>();
+        listChkColor.put(0, (CheckBox) v.findViewById(R.id.chkColorBlack));
+        listChkColor.put(1, (CheckBox) v.findViewById(R.id.chkColorBlue));
+        listChkColor.put(2, (CheckBox) v.findViewById(R.id.chkColorBrown));
+        listChkColor.put(3, (CheckBox) v.findViewById(R.id.chkColorGray));
+        listChkColor.put(4, (CheckBox) v.findViewById(R.id.chkColorGreen));
+        listChkColor.put(5, (CheckBox) v.findViewById(R.id.chkColorPink));
+        listChkColor.put(6, (CheckBox) v.findViewById(R.id.chkColorPurple));
+        listChkColor.put(7, (CheckBox) v.findViewById(R.id.chkColorRed));
+        listChkColor.put(8, (CheckBox) v.findViewById(R.id.chkColorWhite));
+        listChkColor.put(9, (CheckBox) v.findViewById(R.id.chkColorYellow));
+
+        chkBaby = (CheckBox) v.findViewById(R.id.chkIsBaby);
+        chkGenderDiff = (CheckBox) v.findViewById(R.id.chkHasGenderDiff);
+        txtClearFilter = (TextView) v.findViewById(R.id.txtClearFilter);
+    }
+
+    private void toggleFilterArea() {
+        if (scrollFilter.getVisibility() == View.GONE) {
+            scrollFilter.setVisibility(View.VISIBLE);
+            txtShading.setVisibility(View.VISIBLE);
+        } else {
+            scrollFilter.setVisibility(View.GONE);
+            txtShading.setVisibility(View.GONE);
+        }
+    }
+
+    private void toggleVisibility(boolean visibility, SparseArray<CheckBox> listCheckBox, TextView txtFilter) {
+        int new_visibility = visibility ? View.GONE : View.VISIBLE;
+        int drawable_right = visibility ? R.drawable.ic_max_pokedex : R.drawable.ic_min_pokedex;
+
+        txtFilter.setCompoundDrawablesWithIntrinsicBounds(0,0,drawable_right,0);
+        int nList = listCheckBox.size();
+        for (int n = 0; n < nList; n++)
+            listCheckBox.get(n).setVisibility(new_visibility);
+    }
+
+    private void clearFilter() {
+        int nGen = listChkGeneration.size();
+        int nType = listChkType.size();
+        int nColor = listChkColor.size();
+
+        for (int n = 0; n < nGen; n++) listChkGeneration.get(n).setChecked(false);
+        for (int n = 0; n < nType; n++) listChkType.get(n).setChecked(false);
+        for (int n = 0; n < nColor; n++) listChkColor.get(n).setChecked(false);
+
+        chkBaby.setChecked(false);
+        chkGenderDiff.setChecked(false);
+    }
+
+    private void setCLickListenerOnFilter() {
+        txtFilterGeneration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleVisibility(genVisible, listChkGeneration, txtFilterGeneration);
+                genVisible = !genVisible;
+            }
+        });
+
+        txtFilterType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleVisibility(typeVisible, listChkType, txtFilterType);
+                typeVisible = !typeVisible;
+            }
+        });
+
+        txtFilterColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleVisibility(colorVisible, listChkColor, txtFilterColor);
+                colorVisible = !colorVisible;
+            }
+        });
+
+        int nGeneration = listChkGeneration.size();
+        for (int n = 0; n < nGeneration; n++)
+            listChkGeneration.get(n).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFilter();
+            }
+        });
+
+        int nType = listChkType.size();
+        for (int n = 0; n < nType; n++)
+            listChkType.get(n).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setFilter();
+                }
+            });
+
+        int nColor = listChkColor.size();
+        for (int n = 0; n < nColor; n++)
+            listChkColor.get(n).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setFilter();
+                }
+            });
+
+        chkBaby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFilter();
+            }
+        });
+
+        chkGenderDiff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFilter();
+            }
+        });
+
+        txtClearFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!(Name.isEmpty() &&
+                        Generation.isEmpty() &&
+                        Color.isEmpty() &&
+                        !hasGenderDiff &&
+                        !isBaby)) {
+                clearFilter(); setFilter();}
+            }
+        });
+    }
+
+    private void setFilter() {
+        Generation = setFilterText(listChkGeneration);
+        Type = setFilterText(listChkType);
+        Color = setFilterText(listChkColor);
+        isBaby = chkBaby.isChecked();
+        hasGenderDiff = chkGenderDiff.isChecked();
+        makeList();
+    }
+
+    private String setFilterText (SparseArray<CheckBox> listCheckBox) {
+        String out = "";
+        String value;
+        int nList = listCheckBox.size();
+
+        for (int n = 0; n < nList; n++) if (listCheckBox.get(n).isChecked()) {
+            value = String.valueOf(n + 1);
+            if (out.isEmpty()) out += value;
+            else out += ", " + value;
+        }
+
+        return out;
     }
 }
