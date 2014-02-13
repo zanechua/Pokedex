0bd229c12284cd94d95b7f158b370611fe3ad570
Acrophobic <m.radhi.f@gmail.com>
2014-02-13 16:36:12 +0700

    Adapter for list of Pokemon's form

--- src/com/radhi/Pokedex/adapter/ListFormAdapter.java
+++ src/com/radhi/Pokedex/adapter/ListFormAdapter.java
@@ -6,58 +6,71 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.radhi.Pokedex.R;
 import com.radhi.Pokedex.other.Database;
-import com.radhi.Pokedex.other.Enum.ImgSize;
+import com.radhi.Pokedex.other.Other;
+import com.radhi.Pokedex.other.Other.pokemonInterface;
 
 public class ListFormAdapter extends ArrayAdapter<String> {
     private final Context context;
-    private final String[] listName;
+    private final String[] listForm;
+    private final int nForm;
+    private final boolean formSwitchable;
+    private pokemonInterface pokemonInterface;
 
-    public ListFormAdapter(Context context, String[] listName) {
-        super(context, R.layout.row_pokemon_name, listName);
+    public ListFormAdapter(Context context, String[] listForm, boolean formSwitchable) {
+        super(context, R.layout.row_pokemon_form, listForm);
 
         this.context = context;
-        this.listName = listName;
-    }
-
-    static class viewHolder {
-        public TextView txtRowID;
-        public TextView txtRowName;
-        public TextView txtRowType1;
-        public TextView txtRowType2;
-        public ImageView imgRow;
+        this.listForm = listForm;
+        this.formSwitchable = formSwitchable;
+        pokemonInterface = (pokemonInterface) context;
+        nForm = listForm.length;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View rowView = convertView;
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.row_pokemon_form, null);
 
-        if (rowView == null) {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            rowView = inflater.inflate(R.layout.row_pokemon_name, null);
+        LinearLayout rowForm = (LinearLayout) convertView.findViewById(R.id.rowForm);
+        TextView txtRowName = (TextView) convertView.findViewById(R.id.txtRowNama);
+        TextView txtRowType1 = (TextView) convertView.findViewById(R.id.txtRowType1);
+        TextView txtRowType2 = (TextView) convertView.findViewById(R.id.txtRowType2);
+        ImageView imgRow = (ImageView) convertView.findViewById(R.id.imgRowIcon);
 
-            viewHolder v = new viewHolder();
-            v.txtRowID = (TextView) rowView.findViewById(R.id.txtRowID);
-            v.txtRowName = (TextView) rowView.findViewById(R.id.txtRowNama);
-            v.txtRowType1 = (TextView) rowView.findViewById(R.id.txtRowType1);
-            v.txtRowType2 = (TextView) rowView.findViewById(R.id.txtRowType2);
-            v.imgRow = (ImageView) rowView.findViewById(R.id.imgRowIcon);
-            rowView.setTag(v);
+        final String[] dataRow = listForm[position].split(Database.SPLIT);
+        txtRowName.setText(dataRow[1]);
+
+        int type1 = Integer.valueOf(dataRow[2]);
+        txtRowType1.setText(Other.getTypeName(type1));
+        txtRowType1.setTextColor(Other.getTypeColor(type1));
+        txtRowType1.setCompoundDrawablesWithIntrinsicBounds(Other.getTypeImage(type1),0,0,0);
+
+        int type2 = Integer.valueOf(dataRow[3]);
+        if (type2 > 0) {
+            txtRowType2.setText(Other.getTypeName(type2));
+            txtRowType2.setTextColor(Other.getTypeColor(type2));
+            txtRowType2.setCompoundDrawablesWithIntrinsicBounds(Other.getTypeImage(type2),0,0,0);
         }
 
-        viewHolder holder = (viewHolder) rowView.getTag();
+        Other.setImage(imgRow,"sprites/normal/front/nf_" + dataRow[0] + ".png",R.drawable.unknown_small);
 
-        String[] dataRow = listName[position].split(Database.SPLIT);
+        if (position == nForm-1) rowForm.setBackgroundResource(R.drawable.red_filter_title);
 
-        holder.txtRowID.setVisibility(View.GONE);
-        holder.txtRowName.setText(dataRow[1]);
-        Database.setTypeName(holder.txtRowType1, Integer.valueOf(dataRow[2]));
-        Database.setTypeName(holder.txtRowType2, Integer.valueOf(dataRow[3]));
-        Database.setImage(holder.imgRow, "sprites/normal/front/nf_" + dataRow[0] + ".png", ImgSize.SMALL);
+        if (position != 0) {
+            rowForm.setClickable(true);
+            rowForm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pokemonInterface.formSelected(dataRow[4], dataRow[0], dataRow[1], formSwitchable);
+                }
+            });
+        }
 
-        return rowView;
+        return convertView;
     }
 }
