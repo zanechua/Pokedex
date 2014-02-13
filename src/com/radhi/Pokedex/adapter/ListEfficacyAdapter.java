48009b8b22ee93a7fa221b7a8728c2b365cc09d7
Acrophobic <m.radhi.f@gmail.com>
2014-02-13 16:34:54 +0700

    Adapter for list Pokemon's type efficacy

--- src/com/radhi/Pokedex/adapter/ListEfficacyAdapter.java
+++ src/com/radhi/Pokedex/adapter/ListEfficacyAdapter.java
@@ -5,49 +5,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.radhi.Pokedex.R;
 import com.radhi.Pokedex.other.Database;
+import com.radhi.Pokedex.other.Other;
 
 public class ListEfficacyAdapter extends ArrayAdapter<String> {
     private final Context context;
-    private final String[] efficacy;
+    private final String[] efficacyData;
+    private final int nType;
 
-    public ListEfficacyAdapter(Context context, String[] efficacy) {
-        super(context, R.layout.row_pokemon_dex, efficacy);
-
+    public ListEfficacyAdapter(Context context, String[] efficacyData) {
+        super(context, R.layout.row_ability, efficacyData);
         this.context = context;
-        this.efficacy = efficacy;
-    }
-
-    static class viewHolder {
-        public TextView txtValEfficacy;
+        this.efficacyData = efficacyData;
+        nType = efficacyData.length;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View rowView = convertView;
-        if (rowView == null) {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            rowView = inflater.inflate(R.layout.row_pokemon_efficacy, null);
+        String[] item = efficacyData[position].split(Database.SPLIT);
 
-            viewHolder v = new viewHolder();
-            v.txtValEfficacy = (TextView) rowView.findViewById(R.id.txtValEfficacy);
-            rowView.setTag(v);
-        }
-        viewHolder holder = (viewHolder) rowView.getTag();
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.row_type, null);
 
-        String[] rowData = efficacy[position].split(Database.SPLIT);
-        Database.setTypeName(holder.txtValEfficacy, Integer.valueOf(rowData[0]));
-        String currentText = holder.txtValEfficacy.getText().toString();
-        String value;
-        if (rowData[1].equals("0.5")) value = "½";
-        else if (rowData[1].equals("0.25")) value = "¼";
-        else value = rowData[1];
+        LinearLayout rowType = (LinearLayout) convertView.findViewById(R.id.rowType);
+        TextView txtType = (TextView) convertView.findViewById(R.id.txtTypeName);
+        TextView txtValue = (TextView) convertView.findViewById(R.id.txtTypeValue);
 
-        holder.txtValEfficacy.setText(currentText + " " + value + " ×");
+        int type_id = Integer.valueOf(item[0]);
+        String type_name = context.getString(Other.getTypeName(type_id));
 
-        return rowView;
+        txtType.setText(type_name);
+        txtType.setCompoundDrawablesWithIntrinsicBounds(0, 0, Other.getTypeImage(type_id), 0);
+        txtValue.setText("× " + item[1]);
+        txtValue.setTextColor(Other.getTypeColor(type_id));
+
+        if (position == nType-1) rowType.setBackgroundResource(android.R.color.transparent);
+        return convertView;
     }
 }
