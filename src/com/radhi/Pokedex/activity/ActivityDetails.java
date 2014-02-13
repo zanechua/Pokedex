d2f0e8bc1cea6ec7f62c43f50b34e65307d1763e
Acrophobic <m.radhi.f@gmail.com>
2014-02-13 16:25:49 +0700

    For displaying fragment PokemonDetails

--- src/com/radhi/Pokedex/activity/ActivityDetails.java
+++ src/com/radhi/Pokedex/activity/ActivityDetails.java
@@ -1,81 +1,59 @@
 package com.radhi.Pokedex.activity;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
 import com.radhi.Pokedex.R;
-import com.radhi.Pokedex.fragment.*;
-import com.radhi.Pokedex.object.Pokemon;
-import com.radhi.Pokedex.other.PagerAdapter;
+import com.radhi.Pokedex.fragment.PokemonDetails;
+import com.radhi.Pokedex.other.Other;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
+public class ActivityDetails extends FragmentActivity implements Other.pokemonInterface {
 
-public class ActivityDetails extends FragmentActivity {
-    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_details);
+        getWindow().setBackgroundDrawable(null);
 
-        Intent i = getIntent();
-        String ID = i.getStringExtra(ActivityMain.POKEMON_ID);
-        String Name = i.getStringExtra(ActivityMain.POKEMON_NAME);
-        this.setTitle(Name);
+        if (savedInstanceState == null) {
+            PokemonDetails pokemonDetails = new PokemonDetails();
+            pokemonDetails.setArguments(getIntent().getExtras());
 
-        makePage mPage = new makePage();
-        mPage.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,ID);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container,pokemonDetails)
+                    .commit();
+        }
     }
 
-    private class makePage extends AsyncTask<String, Void, Void> {
-        ProgressDialog ringProgressDialog;
-        List<Fragment> fragmentList;
-        List<String> titleList;
+    @Override
+    public void pokemonSelected(String id) {
+        PokemonDetails pokemonDetails = new PokemonDetails();
 
-        @Override
-        protected void onPreExecute() {
-            ringProgressDialog = ProgressDialog.show(ActivityDetails.this, "Please wait...", "Loading data...");
-        }
+        Bundle args = new Bundle();
+        args.putString(Other.PokemonId, id);
+        pokemonDetails.setArguments(args);
 
-        @Override
-        protected Void doInBackground(String... ID) {
-            Pokemon pokemon = new Pokemon(getBaseContext(),ID[0]);
-            Bundle args = new Bundle();
-            args.putParcelable(ActivityMain.POKEMON_DATA,pokemon);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, pokemonDetails)
+                .addToBackStack(null)
+                .commit();
+    }
 
-            fragmentList = new Vector<Fragment>();
-            fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonAppearance.class.getName(), args));
-            fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonData.class.getName(), args));
-            fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonStat.class.getName(), args));
-            fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonMove.class.getName(), args));
+    @Override
+    public void formSelected(String id, String img_id, String name, boolean formSwitchable) {
+        Bundle args = new Bundle();
+        args.putString(Other.PokemonId, id);
+        args.putString(Other.PokemonImageId, img_id);
+        args.putString(Other.PokemonName, name);
 
-            titleList = new ArrayList<String>();
-            titleList.add("APPEARANCE");
-            titleList.add("DATA");
-            titleList.add("STATS");
-            titleList.add("MOVE");
+        PokemonDetails alternativeForm = new PokemonDetails();
+        alternativeForm.setArguments(args);
 
-            if (pokemon.OtherForm().length > 1) {
-                fragmentList.add(Fragment.instantiate(getBaseContext(), PokemonForm.class.getName(), args));
-                titleList.add("FORM");
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            PagerAdapter mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), fragmentList, titleList);
-            ViewPager mPager = (ViewPager) findViewById(R.id.pager);
-            mPager.setAdapter(mPagerAdapter);
-            mPager.setOffscreenPageLimit(5);
-            getWindow().setBackgroundDrawable(null);
-            ringProgressDialog.dismiss();
-        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, alternativeForm)
+                .addToBackStack(null)
+                .commit();
     }
 }
