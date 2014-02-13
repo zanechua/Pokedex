d503e411af4c790a4859d2abea0dc8593d94e016
Acrophobic <m.radhi.f@gmail.com>
2014-02-13 16:37:55 +0700

    Change class Pokemon from Parcelable to normal object

--- src/com/radhi/Pokedex/object/Pokemon.java
+++ src/com/radhi/Pokedex/object/Pokemon.java
@@ -1,246 +1,182 @@
 package com.radhi.Pokedex.object;
 
 import android.content.Context;
-import android.os.Parcel;
-import android.os.Parcelable;
 import com.radhi.Pokedex.other.Database;
-import com.radhi.Pokedex.other.Enum.Effectiveness;
 
-public class Pokemon implements Parcelable {
-    private String ID, EnglishName, JapaneseName, RomajiName, Species,
+public class Pokemon  {
+    public String ID, EnglishName, JapaneseName, RomajiName, Species,
             Habitat, Type1, Type2, Height, Weight, BodyShape, Gender,
             EggGroups, HatchCounter, GrowthRate, CaptureRate,
-            BaseExperience, BaseEffort, BaseHappiness;
+            BaseExperience, BaseEffort, BaseHappiness, EvolutionChainId;
 
-    private String[] DexNumber, Description, Ability, Stats,
-            TypeOffenceEffective, TypeOffenceImmune, TypeOffenceWeak,
-            TypeDefenceWeak, TypeDefenceImmune, TypeDefenceStrong,
-            AvailableMoveVersion, AvailableMoveMethod, OtherForm;
+    public String[] DexNumber, Description, Ability, Stats, MaxStats,
+            TypeEfficacy, VersionGroup, MoveMethod,
+            OtherForm, Evolution, Version, EncounterMethod;
 
-    private Boolean hasGenderDifferences, formSwitchable;
-
-    @Override
-    public int describeContents() {return 0;}
-
-    @Override
-    public void writeToParcel(Parcel out, int flags) {
-        out.writeString(ID);
-        out.writeString(EnglishName);
-        out.writeString(JapaneseName);
-        out.writeString(RomajiName);
-        out.writeString(Species);
-        out.writeString(Habitat);
-        out.writeString(Type1);
-        out.writeString(Type2);
-        out.writeString(Height);
-        out.writeString(Weight);
-        out.writeString(BodyShape);
-        out.writeString(Gender);
-        out.writeString(EggGroups);
-        out.writeString(HatchCounter);
-        out.writeString(GrowthRate);
-        out.writeString(CaptureRate);
-        out.writeString(BaseExperience);
-        out.writeString(BaseEffort);
-        out.writeString(BaseHappiness);
-        out.writeStringArray(DexNumber);
-        out.writeStringArray(Description);
-        out.writeStringArray(Ability);
-        out.writeStringArray(Stats);
-        out.writeStringArray(TypeOffenceEffective);
-        out.writeStringArray(TypeOffenceImmune);
-        out.writeStringArray(TypeOffenceWeak);
-        out.writeStringArray(TypeDefenceWeak);
-        out.writeStringArray(TypeDefenceImmune);
-        out.writeStringArray(TypeDefenceStrong);
-        out.writeStringArray(AvailableMoveVersion);
-        out.writeStringArray(AvailableMoveMethod);
-        out.writeStringArray(OtherForm);
-        out.writeByte((byte) (hasGenderDifferences ? 1 : 0));
-        out.writeByte((byte) (formSwitchable ? 1 : 0));
-    }
-
-    public static final Parcelable.Creator<Pokemon> CREATOR = new Parcelable.Creator<Pokemon>() {
-        @Override
-        public Pokemon createFromParcel(Parcel in) {return new Pokemon(in);}
-
-        @Override
-        public Pokemon[] newArray(int size) {return new Pokemon[size];}
-    };
-
-    private Pokemon(Parcel in) {
-        ID = in.readString();
-        EnglishName = in.readString();
-        JapaneseName = in.readString();
-        RomajiName = in.readString();
-        Species = in.readString();
-        Habitat = in.readString();
-        Type1 = in.readString();
-        Type2 = in.readString();
-        Height = in.readString();
-        Weight = in.readString();
-        BodyShape = in.readString();
-        Gender = in.readString();
-        EggGroups = in.readString();
-        HatchCounter = in.readString();
-        GrowthRate = in.readString();
-        CaptureRate = in.readString();
-        BaseExperience = in.readString();
-        BaseEffort = in.readString();
-        BaseHappiness = in.readString();
-        DexNumber = in.createStringArray();
-        Description = in.createStringArray();
-        Ability = in.createStringArray();
-        Stats = in.createStringArray();
-        TypeOffenceEffective = in.createStringArray();
-        TypeOffenceImmune = in.createStringArray();
-        TypeOffenceWeak = in.createStringArray();
-        TypeDefenceWeak = in.createStringArray();
-        TypeDefenceImmune = in.createStringArray();
-        TypeDefenceStrong = in.createStringArray();
-        AvailableMoveVersion = in.createStringArray();
-        AvailableMoveMethod = in.createStringArray();
-        OtherForm = in.createStringArray();
-        hasGenderDifferences = in.readByte() != 0;
-        formSwitchable = in.readByte() != 0;
-    }
+    public Boolean hasGenderDifferences, hasDexNumber, hasDescription,
+            hasAbility, hasStats, hasEfficacy, formSwitchable,
+            hasOtherForm, hasEvolution, hasLocation, hasMove;
 
     public Pokemon(Context context, String ID) {
         this.ID = ID;
         Database DB = new Database(context);
 
-        String[] name = DB.getPokemonName(ID).split(Database.SPLIT);
-        String[] type = DB.getPokemonType(ID).split(Database.SPLIT);
-        String[] hwe = DB.getPokemonHWE(ID).split(Database.SPLIT);
-        String[] gchgf = DB.getPokemonGCHHGF(ID).split(Database.SPLIT);
+//      Pokemon's name
+        String[] array_name;
+        String name = DB.getPokemonName(ID);
 
-        EnglishName = name[0];
-        JapaneseName = name[1];
-        RomajiName = name[2];
-        Species = name[3];
-        Habitat = DB.getPokemonHabitat(ID);
-        Type1 = type[0];
-        Type2 = type[1];
-        Height = hwe[0];
-        Weight = hwe[1];
-        BaseExperience = hwe[2];
-        BodyShape = DB.getPokemonShape(ID);
-        GrowthRate = DB.getPokemonGrowthRate(ID);
-        Gender = gchgf[0];
-        CaptureRate = gchgf[1];
-        BaseHappiness = gchgf[2];
-        HatchCounter = gchgf[3];
-        hasGenderDifferences = gchgf[4].equals("1");
-        formSwitchable = gchgf[5].equals("1");
-        EggGroups = DB.getPokemonEgg(ID);
-        BaseEffort = DB.getPokemonBaseEffort(ID);
+        if (name != null) array_name = name.split(Database.SPLIT);
+        else array_name = new String[] {"","","",""};
+
+        EnglishName = array_name[0];
+        JapaneseName = array_name[1];
+        RomajiName = array_name[2];
+        Species = array_name[3].isEmpty() ? "" :
+                array_name[3] + " Pokémon";
+
+//      Pokemon's habitat
+        String habitat = DB.getPokemonHabitat(ID);
+        Habitat = habitat != null ? habitat : "";
+
+//      Pokemon's type
+        String[] array_type;
+        String type = DB.getPokemonType(ID);
+
+        if (type != null) array_type = type.split(Database.SPLIT);
+        else array_type = new String[] {"0","0"};
+
+        Type1 = array_type[0];
+        Type2 = array_type[1];
+
+//      Pokemon's height, weight, and base_experience
+        String[] array_HWE;
+        String HWE = DB.getPokemonHWE(ID);
+
+        if (HWE != null) array_HWE = HWE.split(Database.SPLIT);
+        else array_HWE = new String[] {"0","0","0"};
+
+        Height = getHeight(Double.parseDouble(array_HWE[0]));
+        Weight = getWeight(Double.parseDouble(array_HWE[1]));
+        BaseExperience = array_HWE[2];
+
+//      Pokemon's body shape
+        String shape = DB.getPokemonShape(ID);
+        BodyShape = shape != null ? shape : "Unknown";
+
+//      Pokemon's growth rate
+        String growth = DB.getPokemonGrowthRate(ID);
+        GrowthRate = growth != null ? growth : "Unknown";
+
+//      Pokemon's gender ratio, capture rate, base happiness, etc
+        String[] array_GCHHGF;
+        String GCHHGF = DB.getPokemonGCHHGF(ID);
+
+        if (GCHHGF != null) array_GCHHGF = GCHHGF.split(Database.SPLIT);
+        else array_GCHHGF = new String[] {"-2","0","0","0","0","0","0"};
+
+        Gender = getGenderRatio(Integer.valueOf(array_GCHHGF[0]));
+        CaptureRate = getCaptureRate(Integer.valueOf(array_GCHHGF[1]));
+        BaseHappiness = array_GCHHGF[2];
+        HatchCounter = getHatchCounter(Integer.valueOf(array_GCHHGF[3]));
+        hasGenderDifferences = array_GCHHGF[4].equals("1");
+        formSwitchable = array_GCHHGF[5].equals("1");
+        EvolutionChainId = array_GCHHGF[6];
+
+//      Pokemon's egg group
+        String egg = DB.getPokemonEgg(ID);
+        EggGroups = egg != null ? egg : "Unknown";
+
+//      Pokemon's base effort
+        String effort = DB.getPokemonBaseEffort(ID);
+        BaseEffort = effort != null ? effort : "-";
+
+//      Pokemon's dex number
         DexNumber = DB.getPokedexNumber(ID);
+        hasDexNumber = DexNumber.length > 0;
+
+//      Pokemon's description
         Description = DB.getPokemonDescription(ID);
+        hasDescription = Description.length > 0;
+
+//      Pokemon's ability
         Ability = DB.getPokemonAbility(ID);
+        hasAbility = Ability.length > 0;
+
+//      Pokemon stats
         Stats = DB.getPokemonStat(ID);
-        TypeDefenceWeak = DB.getPokemonEfficacy(Type1, Type2, Effectiveness.SUPER_EFFECTIVE);
-        TypeDefenceImmune = DB.getPokemonEfficacy(Type1, Type2, Effectiveness.IMMUNE);
-        TypeDefenceStrong = DB.getPokemonEfficacy(Type1, Type2, Effectiveness.NOT_EFFECTIVE);
-        AvailableMoveVersion = DB.getPokemonMoveVersion(ID);
-        AvailableMoveMethod = DB.getPokemonMoveMethod(ID);
-        OtherForm = DB.getPokemonForm(ID,EnglishName);
+        MaxStats = DB.getPokemonMaxStat(ID, Stats);
+        hasStats = Stats.length > 0;
+
+//      Pokemon's efficacy
+        TypeEfficacy = DB.getPokemonEfficacy(Type1, Type2);
+        hasEfficacy = TypeEfficacy.length > 0;
+
+//      Pokemon's alternative form
+        OtherForm = DB.getPokemonForm(ID, EnglishName);
+        hasOtherForm = OtherForm.length > 1;
+
+//      Pokemon's evolution
+        Evolution = DB.getPokemonEvolution(EvolutionChainId);
+        hasEvolution = Evolution.length > 1;
+
+//      Pokemon's location
+        Version = DB.getPokemonVersion(ID);
+        EncounterMethod = DB.getPokemonEncounterMethod(ID);
+        hasLocation = Version.length > 0;
+
+//      Pokemon's move
+        VersionGroup = DB.getPokemonVersionGroup(ID);
+        MoveMethod = DB.getPokemonMoveMethod(ID);
+        hasMove = VersionGroup.length > 0;
 
         DB.close();
     }
 
-    public String ID() {return ID;}
+    private String getHeight(double height) {
+        int cm = (int) (height * 10);
+        int ft = (int) (cm / 30.48);
+        double inch = (cm - (ft * 30.48)) / 2.54;
 
-    public String EnglishName() {return EnglishName;}
+        String metric;
+        if (cm < 100) metric = String.valueOf(cm) + " cm";
+        else metric = String.format("%s", cm/100d) + " m";
 
-    public String JapaneseName() {return JapaneseName;}
+        String british = String.valueOf(ft) + "' " +
+                String.format("%.1f", inch) + "\"";
 
-    public String RomajiName() {return RomajiName;}
+        return metric + "\n" + british;
+    }
 
-    public String Species() {return Species + " Pokémon";}
+    private String getWeight(double weight) {
+        double kg = weight / 10;
+        double lb = kg * 2.20462262;
+        return String.format("%s", kg) + " kg\n" +
+                String.format("%.2f",lb) + " lbs";
+    }
 
-    public String Habitat() {return Habitat;}
+    private String getGenderRatio(int value) {
+        int male = 8 - value;
 
-    public String Type1() {return Type1;}
+        if (value == -2) return "Unknown";
+        else if (value == -1) return "Genderless";
+        else return String.format("%s", male / 8d * 100) + "% ♂, "
+                + String.format("%s", value / 8d * 100) + "% ♀";
+    }
 
-    public String Type2() {return Type2;}
-
-    public String Height() {
-        if (Height.equals("-1")) return Database.UNKNOWN;
+    public String getCaptureRate(int value) {
+        if (value == - 1) return "Unknown";
         else {
-            double cm = Double.parseDouble(Height) * 10;
-            int ft = (int)(cm/30.48);
-            double inch = (cm - (ft * 30.48)) / 2.54;
-
-            String metric;
-            if (cm < 100) metric = String.format("%s", cm) + "cm";
-            else metric = String.format("%s", cm / 100) + "m";
-
-            return metric + " (" + String.valueOf(ft) + "'" + String.format("%.1f",inch) + "'')";
+            double percent = value / 765d * 100;
+            return String.valueOf(value) +
+                    " (" + String.format("%.2f",percent) + "%) " +
+                    "Pokéball, full HP";
         }
     }
 
-    public String Weight() {
-        if (Weight.equals("-1")) return Database.UNKNOWN;
-        else {
-            double kg = Double.parseDouble(Weight) / 10;
-            double lb = kg * 2.20462262;
-            return String.format("%s", kg) + "kg (" + String.format("%.2f", lb) + "lbs)";
-        }
+    public String getHatchCounter(int value) {
+        int steps = value * 255;
+
+        return String.valueOf(value) + " (" +
+                String.valueOf(steps) + " steps)";
     }
-
-    public String BodyShape() {return BodyShape;}
-
-    public String Gender() {
-        double female = Double.parseDouble(Gender);
-        double male = 8 - female;
-
-        if (female == -2) return Database.UNKNOWN;
-        else if (female == -1) return "Genderless";
-        else return String.format("%s",male / 8 * 100) + "% ♂, "
-                + String.format("%s",female / 8 * 100) + "% ♀";
-    }
-
-    public String EggGroups() {return EggGroups;}
-
-    public String HatchCounter() {return HatchCounter;}
-
-    public String GrowthRate() {return GrowthRate;}
-
-    public String CaptureRate() {
-        if (CaptureRate.equals("-1")) return Database.UNKNOWN;
-        else {
-            double percent = Double.parseDouble(CaptureRate) / 765 * 100;
-            return CaptureRate + " (" + String.format("%.2f",percent) + "% with PokéBall, full HP)";
-        }
-    }
-
-    public String BaseExperience() {return BaseExperience;}
-
-    public String BaseEffort() {return BaseEffort;}
-
-    public String BaseHappiness() {return BaseHappiness;}
-
-    public Boolean hasGenderDifferences() {return hasGenderDifferences;}
-
-    public Boolean FormSwitchable() {return formSwitchable;}
-
-    public String[] DexNumber() {return DexNumber;}
-
-    public String[] Description() {return Description;}
-
-    public String[] Ability() {return Ability;}
-
-    public String[] Stats() {return Stats;}
-    
-    public String[] TypeDefenceWeak() {return TypeDefenceWeak;}
-
-    public String[] TypeDefenceImmune() {return TypeDefenceImmune;}
-
-    public String[] TypeDefenceStrong() {return TypeDefenceStrong;}
-    
-    public String[] AvailableMoveVersion() {return AvailableMoveVersion;}
-
-    public String[] AvailableMoveMethod() {return AvailableMoveMethod;}
-
-    public String[] OtherForm() {return OtherForm;}
 }
